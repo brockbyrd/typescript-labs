d0aa6a295c60aa86aee2ac2cca775dd60d3523b5
brockbyrd <brockbyrdd@gmail.com>
2021-02-16 11:37:20 -0600

    more value declarations starting with null

--- main.ts
+++ main.ts
@@ -15,3 +15,21 @@ let name: string = "Brock";
 let sentence: string = `My name is ${name} I am a beginner in TypeScript.`;
 
 console.log(sentence);
+
+let n: null = null;
+
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
+
+let person1: [string, number] = ["Brock", 22]
+
+enum Color {Red, Green, Blue};
+
+let c: Color = Color.Green;
+
+console.log(c)
